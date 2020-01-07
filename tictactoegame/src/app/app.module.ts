d930077b857c3466718d3e71c2cfa1ab912a73a9
akash20roy <akash20roy@gmail.com>
2020-01-07 09:13:16 +0530

    tictactoe game

---
+++ tictactoegame/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { BoardComponent } from './component/board/board.component';
+import { SquareComponent } from './component/square/square.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BoardComponent,
+    SquareComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
